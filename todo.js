0084d2484cdafb280a2c0151154e38dfd7c0b49f
Manahil Ameer <manahil1054@gmail.com>
2021-11-27 19:38:59 +0500

    Update

--- todo.js
+++ todo.js
@@ -12,7 +12,7 @@ let title = document.getElementById("title");
 title.focus();
 function get_update(input) {
   input = title.value;
-  
+
   if (input == "") {
     alert("You have not entered any Item!!");
   } else if (arr.includes(input)) {
@@ -20,63 +20,54 @@ function get_update(input) {
   } else {
     id++;
     arr.push([input, id, false]);
-    update();
-  }
-}
-function update() {
-  str = "";
-  arr.forEach((element, index) => {
-    const text = element[0];
-    str =
-      str +
-      `<tr>
-        <td ><input type="checkbox" name="check" id="check" onchange="fun(this,${element[1]})"/></td>
-        <td>${text}</td>
-        <td><i class="material-icons" onclick="deleted(${index})">delete</i></td>
+    // update();
+    document.getElementById("tableBody").innerHTML += `<tr>
+        <td ><input type="checkbox" name="check" id="check" onchange="fun(this)"/></td>
+        <td id="text">${input}</td>
+        <td><i class="material-icons" onclick="deleted(this)">delete</i></td>
         </tr>`;
-    
-    
-    document.getElementById("check").checked= element[2];
-    
-  });
-  
-  document.getElementById("tableBody").innerHTML = str;
-  title.value = "";
-  title.focus();
-}
-function fun(element, id) {
-  const pointer = arr[id - 1];
-  if (element.checked) {
-    pointer[2] = true;
-    
-  } else {
-    pointer[2] = false;
-    
+    title.value = "";
+      title.focus();
   }
-  
+}
+// function update() {
+//   str = "";
+//   arr.forEach((element, index) => {
+//     const text = element[0];
+//     str =
+//       str +
+//       `<tr>
+//         <td ><input type="checkbox" name="check" id="check" onchange="fun(this)"/></td>
+//         <td id="text">${text}</td>
+//         <td><i class="material-icons" onclick="deleted(${index})">delete</i></td>
+//         </tr>`;
+//   });
+
+//   document.getElementById("tableBody").innerHTML = str;
+//   title.value = "";
+//   title.focus();
+// }
+function fun(element) {
+  // const pointer = arr[id - 1];
+  if (element.checked) {
+    element.setAttribute("checked", "checked");
+    element.nextElementSibling.style.textDecoration = "line-through";
+    // pointer[2] = true;
+  } else {
+    // pointer[2] = false;
+  }
 }
 function check() {
   arr.forEach((element) => {
-    
     document.getElementById("check").checked = element[2];
     alert(document.getElementById("check").checked);
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-function deleted(item_index) {
-  arr.splice(item_index, 1);
-  
-  update();
+function deleted(element) {
+  element.parentElement.parentElement.remove();
+  // arr.splice(item_index, 1);
+  // update();
 }
 
 btn_add.addEventListener("click", () => {

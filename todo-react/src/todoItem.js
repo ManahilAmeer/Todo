b999393b09eee8ec79bbbec8564b38a18b864a87
ManahilAmeer <55192237+ManahilAmeer@users.noreply.github.com>
2021-12-07 21:05:16 +0500

    Delete todoItem.js

--- todo-react/src/todoItem.js
+++
@@ -1,6 +0,0 @@
-import React, { createRef } from "react";
-import ReactDOM from "react-dom";
-import { MdDelete } from "react-icons/md";
-
-class TodoItem extends React.Component{}
-export default TodoItem;
